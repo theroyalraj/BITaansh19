24a88311df55cfa13d7c4287f936305f280e1afb
Utkarsh Raj <utraj00114@gmail.com>
2019-10-22 18:30:01 +0530

    credits

---
+++ beta/19/js/clicker.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    $('#clickhere').click(function() {
+      
+      if($("#clickhere").is(":visible")){
+                    $('#show').toggle("slide");
+                    $(".hid1").hide(50);
+                } 
+
+    });
+    $('#back').click(function() {
+      
+      if($("#back").is(":visible")){
+      				$("#show").hide(80);
+                    $('.hid1').show(80);
+                    
+                } 
+                
+    });
+
+      $('#clickhere2').click(function() {
+      
+      if($("#clickhere2").is(":visible")){
+                    $('#show2').toggle("slide");
+                    $(".hid2").hide(50);
+                } 
+
+    });
+    $('#back2').click(function() {
+      
+      if($("#back2").is(":visible")){
+      				$("#show2").hide(80);
+                    $('.hid2').show(80);
+                    
+                } 
+                
+    });
+$('#clickhere3').click(function() {
+      
+      if($("#clickhere3").is(":visible")){
+                    $('#show3').toggle("slide");
+                    $(".hid3").hide(50);
+                } 
+
+    });
+    $('#back3').click(function() {
+      
+      if($("#back3").is(":visible")){
+      				$("#show3").hide(80);
+                    $('.hid3').show(80);
+                    
+                } 
+                
+    });
+    $('#clickhere4').click(function() {
+      
+      if($("#clickhere4").is(":visible")){
+                    $('#show4').toggle("slide");
+                    $(".hid4").hide(50);
+                } 
+
+    });
+    $('#back4').click(function() {
+      
+      if($("#back4").is(":visible")){
+      				$("#show4").hide(80);
+                    $('.hid4').show(80);
+                    
+                } 
+                
+    });
+$('#clickhere5').click(function() {
+      
+      if($("#clickhere5").is(":visible")){
+                    $('#show5').toggle("slide");
+                    $(".hid5").hide(50);
+                } 
+
+    });
+    $('#back5').click(function() {
+      
+      if($("#back5").is(":visible")){
+      				$("#show5").hide(80);
+                    $('.hid5').show(80);
+                    
+                } 
+                
+    });
+
+
+});
+
+ 
